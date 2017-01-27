80d4e3e07d818e1dfd50c3f57062fba8581bcae1
Keith Jackson <kj5thguitar@gmail.com>
2017-01-27 14:56:01 -0500

    added dirived classes

---
+++ BankAccountProject/Checking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountProject
+{
+    class Checking
+    {
+        //Fields
+        private double checkingBalance;
+        private double interest;
+    }
+}
